6b3feda43b01d48d0a1ccc1d2026e6535edae0be
Kacper Wąsik <kacper@wasik.me>
2023-05-07 17:12:32 +0200

    fix workspaces

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -1,24 +1,17 @@
-import {$, cd} from 'zx';
+import {$} from 'zx';
 import {glob} from 'glob';
 
 describe('Astro build', () => {
-  afterEach(async () => {
-    cd('../app');
-
-    await $`rm -rf dist/`;
-  });
-
   test('full project build', async () => {
-    cd('../app');
     await $`npm run build`;
 
-    const files = await glob('../app/dist/**/*.html');
+    const files = await glob('app/dist/**/*.html');
 
     expect(files).toEqual([
-      'dist/index.html',
-      'dist/blog/post-3.html',
-      'dist/blog/post-2.html',
-      'dist/blog/post-1.html',
+      'app/dist/index.html',
+      'app/dist/blog/post-3.html',
+      'app/dist/blog/post-2.html',
+      'app/dist/blog/post-1.html',
     ]);
   });
 
@@ -26,26 +19,24 @@ describe('Astro build', () => {
     test("creates only index.html (ROUTE='/')", async () => {
       process.env.ROUTE = '/';
 
-      cd('../app');
       await $`npm run build`;
 
-      const files = await glob('../app/dist/**/*.html');
+      const files = await glob('app/dist/**/*.html');
 
-      expect(files).toEqual(['dist/index.html']);
+      expect(files).toEqual(['app/dist/index.html']);
     });
 
     test("creates three blog posts (ROUTE='/blog/[slug]')", async () => {
       process.env.ROUTE = '/blog/[slug]';
 
-      cd('../app');
       await $`npm run build`;
 
-      const files = await glob('../app/dist/**/*.html');
+      const files = await glob('app/dist/**/*.html');
 
       expect(files).toEqual([
-        'dist/blog/post-3.html',
-        'dist/blog/post-2.html',
-        'dist/blog/post-1.html',
+        'app/dist/blog/post-3.html',
+        'app/dist/blog/post-2.html',
+        'app/dist/blog/post-1.html',
       ]);
     });
   });
@@ -54,11 +45,10 @@ describe('Astro build', () => {
     process.env.ROUTE = '/blog/[slug]';
     process.env.PARAMS = JSON.stringify({slug: 'post-4'});
 
-    cd('../app');
     await $`npm run build`;
 
-    const files = await glob('../app/dist/**/*.html');
+    const files = await glob('app/dist/**/*.html');
 
-    expect(files).toEqual(['dist/blog/post-4.html']);
+    expect(files).toEqual(['app/dist/blog/post-4.html']);
   });
 });
